8d601663db99b668686ec2b9333e7d3abaa61976
Kanoa Matton <kanoa.matton@atmosphere.tv>
2023-10-25 21:53:18 -0500

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-  import Notes from './components/Notes.vue'
-  //
-  
+  import Notes from './components/Notes.vue'  
 </script>
 
 <template>
-  <Notes></Notes>
+  <!-- <Notes></Notes> -->
+  <text>Yurrrrrr Kai</text>
 </template>
+
+<style>
+    text {
+        color: white;
+    }
+</style>
